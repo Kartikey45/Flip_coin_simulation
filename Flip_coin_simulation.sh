af90e336030d2dec0ff748a589d020d51ededa45
Kartikey45 <singhkartikey45@gmail.com>
2020-02-18 10:59:35 +0530

    ADDED:Percentage of head and tail of the singlet combination

--- Flip_coin_simulation.sh
+++ Flip_coin_simulation.sh
@@ -2,13 +2,34 @@
 
 echo "Welcome"
 
-#INITIALIZE THE RANDOM VALUE
-random=$(( RANDOM%2 ))
+#VARIABLE INITIALIZED
+numberOfTimesHead=0
+numberOfTimesTail=0
 
-#CHECK THE CONDITION AND DISPLAY HEAD OR TAIL
-if [ $random -eq 1 ]
-then
-	echo "head"
-else
-	echo "tail"
-fi
+#TAKE USER INPUT
+read -p "Enter the limit of number of times coin flips:" limit
+
+#STORE VALUES IN THE DICTIONARY
+declare -A dict 
+for((i=1; i<=limit; i++))
+do
+	if [ $(( RANDOM%2 )) -eq 1 ]
+	then
+		dict[$i]=H
+		(( numberOfTimesHead++ ))
+	else
+		dict[$i]=T
+		(( numberOfTimesTail++ ))
+	fi
+done
+
+#DISPLAY VALUES IN THE DICTIONARY
+echo "values in the dictionary : ${dict[@]}"
+
+#CALCULATE PERCENTAGE OF HEAD AND TAIL
+headPercent=$(echo "scale=2;$numberOfTimesHead*100/$limit"|bc)
+tailPercent=$(echo "scale=2;$numberOfTimesTail*100/$limit"|bc)
+
+#DISPLAY PERCENTAGE OF HEAD AND TAIL
+echo "head percent = $headPercent %"
+echo "tail percent = $tailPercent %"
